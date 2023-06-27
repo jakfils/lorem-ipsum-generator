5da121188988893a08ef63441f9c60d6cfd3c06b
jakfils <jakfilstechno@gmail.com>
2023-06-27 16:04:15 +0100

    Add copy behaviour

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
+const copyBtn = document.getElementById("copy-button");
+const copyMessage = document.getElementById("copy-message");
 
+// Function show message for n seconds
+function showMessage(numberOfSeconds){
+setTimeout((e) => {
+    copyMessage.classList.add("invisible");
+  }, numberOfSeconds*1000);
+}
+// Function copy to clipboard
+function copyToClipboard() {
+  let copyText = document.querySelector(".generated-content").textContent;
+  navigator.clipboard.writeText(copyText)
+}
+// Show copy message
+copyBtn.addEventListener("click", (e) => {
+  copyMessage.classList.remove("invisible");
+  copyToClipboard();
+  showMessage(2)
+});
